d6c2e5d46b962dbf717c828ecac650f0b9d6e5c8
Nima Hejazi <nima.hejazi7@gmail.com>
2016-02-06 13:41:19 -0800

    Create _setupPY.sh

---
+++ _setupPY.sh
@@ -0,0 +1,29 @@
+
+# Python2 stuff with Pip
+
+pip install ipython Pillow virtualenv nose notebook
+
+pip install numpy scipy pandas scikit-learn seaborn
+
+pip install nltk sympy q snakeviz networkx cloud
+
+pip install html5lib cssselect pyquery lxml BeautifulSoup
+
+pip install Flask Django tornado
+
+pip install rdflib SPARQLWrapper
+
+
+# Python3 stuff with Pip
+
+pip3 install ipython Pillow virtualenv nose notebook
+
+pip3 install numpy scipy pandas scikit-learn seaborn
+
+pip3 install nltk sympy q snakeviz networkx cloud
+
+pip3 install html5lib cssselect pyquery lxml BeautifulSoup
+
+pip3 install Flask Django tornado
+
+pip3 install rdflib SPARQLWrapper
