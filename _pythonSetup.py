6ebc62628a4bd9b3b937b868f297f435388a0713
Nima Hejazi <nima.hejazi7@gmail.com>
2016-02-26 13:00:29 -0800

    oh, grammar

--- _pythonSetup.py
+++ _pythonSetup.py
@@ -7,7 +7,7 @@ def install(packages):
 # Example
 core = ('ipython','Pillow','virtualenv','nose','notebook','jupyter','xonsh')
 basesci = ('numpy','scipy','pandas','scikit-learn','seaborn','plotly')
-basemath = ('nltk','sympy','q','snakeviz','networkx'.'cloud')
+basemath = ('nltk','sympy','q','snakeviz','networkx','cloud')
 internet = ('html5lib','cssselect','pyquery','lxml','BeautifulSoup')
 otherstuff = ('Flask','Django','tornado','rdflib','SPARQLWrapper')
 neurosci = ('nibabel','nipy','nitime','nilearn')
