aabfda95d0b022da0e269014fd56c8d32ab14c5b
Nima Hejazi <nima.hejazi7@gmail.com>
2016-02-26 12:31:36 -0800

    pythonize install script

---
+++ _setupPython.py
@@ -0,0 +1,21 @@
+import pip
+
+def install(packages):
+    for x in range(len(packages)):
+        pip.main(['install',packages[x]])
+
+# Example
+core = ('ipython','Pillow','virtualenv','nose','notebook','jupyter','xonsh')
+basesci = ('numpy','scipy','pandas','scikit-learn','seaborn','plotly')
+basemath = ('nltk','sympy','q','snakeviz','networkx'.'cloud')
+internet = ('html5lib','cssselect','pyquery','lxml','BeautifulSoup')
+otherstuff = ('Flask','Django','tornado','rdflib','SPARQLWrapper')
+neurosci = ('nibabel','nipy','nitime','nilearn')
+
+if __name__ == '__main__':
+    install(core)
+    install(basesci)
+    install(basemath)
+    install(internet)
+    install(otherstuff)
+    install(neurosci)
