9132166cc80373a0e3583045cec3363a1f1854bb
Nima Hejazi <nima.hejazi7@gmail.com>
2016-02-26 11:40:51 -0800

    separate Python3 install script

---
+++ _setupPython3.sh
@@ -0,0 +1,19 @@
+# Python 3.5+ modules/packages with pip3
+
+pip3 install ipython Pillow virtualenv nose notebook jupyter xonsh
+
+pip3 install numpy scipy pandas scikit-learn seaborn mayavi plotly
+
+pip3 install nltk sympy q snakeviz networkx cloud
+
+pip3 install html5lib cssselect pyquery lxml BeautifulSoup
+
+pip3 install Flask Django tornado
+
+pip3 install rdflib SPARQLWrapper
+
+pip3 install nibabel nipy nitime nilearn
+
+pip3 list --outdated  #to find packages that are out of date
+
+#pip3 install -U package1 package2 ...  #manually update outdated packages
