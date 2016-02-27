9919fdd1899e11e0c692f6c3fb79b0ffd6392501
Nima Hejazi <nima.hejazi7@gmail.com>
2016-02-27 10:03:34 -0800

    remove xonsh

--- _pythonSetup.py
+++ _pythonSetup.py
@@ -5,7 +5,7 @@ def install(packages):
         pip.main(['install',packages[x]])
 
 # Example
-core = ('ipython','Pillow','virtualenv','nose','notebook','jupyter','xonsh')
+core = ('ipython','Pillow','virtualenv','nose','notebook','jupyter')
 basesci = ('numpy','scipy','pandas','scikit-learn','seaborn','plotly')
 basemath = ('nltk','sympy','q','snakeviz','networkx','cloud')
 internet = ('html5lib','cssselect','pyquery','lxml','BeautifulSoup')
