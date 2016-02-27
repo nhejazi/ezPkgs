a25a9b8e57c440924e77db82d81f543a946b9e0a
Nima Hejazi <nima.hejazi7@gmail.com>
2016-02-27 10:03:54 -0800

    remove xonsh until pip stable

--- _pythonUpdate.py
+++ _pythonUpdate.py
@@ -5,7 +5,7 @@ def upgrade(packages):
         pip.main(['install','--upgrade',packages[x]])
 
 # Example
-core = ('ipython','Pillow','virtualenv','nose','notebook','jupyter','xonsh')
+core = ('ipython','Pillow','virtualenv','nose','notebook','jupyter')
 basesci = ('numpy','scipy','pandas','scikit-learn','seaborn','plotly')
 basemath = ('nltk','sympy','q','snakeviz','networkx','cloud')
 internet = ('html5lib','cssselect','pyquery','lxml','BeautifulSoup')
