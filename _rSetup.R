6beb0acb3e59e32f168f1182c79a2fb480e348e4
Nima Hejazi <nima.hejazi7@gmail.com>
2016-03-21 18:58:11 -0700

    add devtools::release dependency rversions

--- _rSetup.R
+++ _rSetup.R
@@ -10,7 +10,7 @@ install.packages(c('car','tidyr','dplyr','pryr','ggplot2','randomForest',
                    'RColorBrewer','neuralnet','nnet','clusterGeneration',
                    'RSNNS','tmle','ltmle','selectiveInference','knitr',
                    'htmltools','yaml','shiny','rmarkdown','png','stringi',
-                   'gridExtra'),
+                   'gridExtra','rversions'),
                  repos='https://cran.cnr.berkeley.edu/')
 
 # for GitHub packages
