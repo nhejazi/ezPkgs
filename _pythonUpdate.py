1d4b2dae1d2d9832ae1409f8574da1b09852f806
Nima Hejazi <nima.hejazi7@gmail.com>
2016-03-23 13:23:50 -0700

    add aws module

--- _pythonUpdate.py
+++ _pythonUpdate.py
@@ -8,7 +8,7 @@ def upgrade(packages):
 core = ('ipython','Pillow','virtualenv','nose','notebook','jupyter','pylint','xonsh')
 basesci = ('numpy','scipy','pandas','scikit-learn','matplotlib','seaborn','plotly')
 basemath = ('nltk','sympy','q','snakeviz','networkx','cloud')
-internet = ('html5lib','cssselect','pyquery','lxml','BeautifulSoup')
+internet = ('awscli','html5lib','cssselect','pyquery','lxml','BeautifulSoup')
 otherstuff = ('Flask','Django','tornado','rdflib','SPARQLWrapper')
 neurosci = ('nibabel','nipy','nitime','nilearn')
 
