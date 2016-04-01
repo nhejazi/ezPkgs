6e8039062cf32269fc9bff8420765bee38c92df8
Nima Hejazi <nima.hejazi7@gmail.com>
2016-03-31 22:13:19 -0700

    mostly renaming things

--- README.md
+++ README.md
@@ -4,22 +4,22 @@
 To set up useful package libraries for Python, Julia, R, and the Atom editor, 
 use the following:
 
-1. for Atom v1.5+: `sh _atomSetup.sh` (uses Atom's `apm`)
-2. for R v3.2+: `R CMD BATCH _rSetup.R _rSetup.Rout; rm _rSetup.Rout`
-3. for Python v3.4+: `python3 _pythonSetup.py` (to update packages, use 
-   `python3 _pythonUpdate.py`)
-4. for Python v2.7+: `python2 _pythonSetup.py` (to update packages, use 
-   `python2 _pythonUpdate.py`)
-5. for Julia v4.3+: `julia _juliaSetup.jl`
+1. Atom v1.5+: `sh install_atompkgs.sh` (uses Atom `apm`)
+2. R v3.2+: `R CMD BATCH install_rpkgs.R install_rpkgs.Rout`
+3. Python v3.4+: `python3 install_pypkgs.py` (to update packages, use 
+   `python3 update_pypkgs.py`)
+4. Python v2.7+: `python2 install_pypkgs.py` (to update packages, use 
+   `python2 update_pypkgs.py`)
+5. Julia v4.3+: `julia install_juliapkgs.jl`
 
 
 _Supplementary_:
 
-1. download and install the [Xonsh 
+1. set up Jupyter kernels for R, Julia, and Python using instructions in 
+   `jupyter_notes.md`
+2. download and install the pythonic [Xonsh 
    shell](https://github.com/scopatz/xonsh.git) by following the instructions 
    provided.
-2. set up Jupyter kernels for Python/Julia/R using instructions in 
-   `_jupyterNotes.md`
 
 N.B., for these scripts to work as intended, _base installations of these tools 
 must be performed in advance._
---
+++ install_rpkgs.R
@@ -0,0 +1,33 @@
+# lists of packages to install, organized by repository
+cran_pkgs <- c('car', 'tidyr', 'dplyr', 'pryr', 'ggplot2', 'randomForest',
+               'SuperLearner', 'lme4', 'nlme', 'stringr', 'lubridate', 'ggvis',
+               'rgl', 'htmlwidgets', 'dygraphs', 'network3D', 'threeJS',
+               'googleVis', 'mgcv', 'multcomp', 'vcd', 'glmnet', 'survival',
+               'caret', 'shiny', 'xtable', 'pander', 'devtools', 'zoo', 'xts',
+               'quantmod', 'data.table', 'Rcpp', 'parallel', 'testthat',
+               'roxygen2', 'foreign', 'ade4', 'scatterplot3d', 'Rcurl', 'DBI',
+               'RMySQL', 'impute', 'fastICA', 'e1071', 'reshape2', 'lintr',
+               'RColorBrewer', 'neuralnet', 'nnet', 'clusterGeneration',
+               'RSNNS', 'tmle', 'ltmle', 'selectiveInference', 'knitr',
+               'htmltools', 'yaml', 'shiny', 'rmarkdown', 'png', 'stringi',
+               'gridExtra', 'rversions', 'R.devices', 'future')
+
+bioc_pkgs <- c("GenomicRanges", "Gviz", "GenomicFeatures", "VariantAnnotation", 
+               "rhdf5", "biomaRt", "limma", "affy", "bsseq", "ggbio", "made4", 
+               "gwascat", "hgu133plus2cdf", "hgu133plus2probe", "Heatplus",
+               "TxDb.Hsapiens.UCSC.hg18.knownGene", "rtracklayer")
+
+github_pkgs <- c("hadley/staticdocs", "ramnathv/slidify", 
+                 "ramnathv/slidifyLibraries", "nhejazi/nima")
+
+# install packages from CRAN
+install.packages(cran_pkgs, repos = "https://cran.cnr.berkeley.edu/")
+
+# install packages from Bioconductor
+source("http://www.bioconductor.org/biocLite.R")
+biocLite(bio_pkgs)
+biocLite(ask = FALSE)
+
+# install packages from GitHub
+library(devtools)
+devtools::install_github(github_pkgs)
