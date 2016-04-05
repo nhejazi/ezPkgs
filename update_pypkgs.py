0c1d5d37e40950b68cfacd853b09596911ccb73d
Nima Hejazi <nima.hejazi7@gmail.com>
2016-04-05 00:14:11 -0700

    add statsmodels, rename lists

--- update_pypkgs.py
+++ update_pypkgs.py
@@ -7,17 +7,17 @@ def upgrade(packages):
 # Example
 core = ('ipython','Pillow','virtualenv','nose','pylint','jupyter',
         'notebook','xonsh','neovim')
-basesci = ('numpy','scipy','pandas','scikit-learn','matplotlib','seaborn',
-           'plotly')
-basemath = ('nltk','sympy','q','snakeviz','networkx','cloud')
-internet = ('awscli','html5lib','cssselect','pyquery','lxml','BeautifulSoup')
-otherstuff = ('Flask','Django','tornado','rdflib','SPARQLWrapper')
-neurosci = ('nibabel','nipy','nitime','nilearn')
+stat = ('numpy','scipy','pandas','scikit-learn','matplotlib','seaborn',
+        'plotly','statsmodels')
+math = ('nltk','sympy','q','snakeviz','networkx','cloud')
+web = ('awscli','html5lib','cssselect','pyquery','lxml','BeautifulSoup')
+misc = ('Flask','Django','tornado','rdflib','SPARQLWrapper')
+neuro = ('nibabel','nipy','nitime','nilearn')
 
 if __name__ == '__main__':
     upgrade(core)
-    upgrade(basesci)
-    upgrade(basemath)
-    upgrade(internet)
-    upgrade(otherstuff)
-    upgrade(neurosci)
+    upgrade(stat)
+    upgrade(math)
+    upgrade(web)
+    upgrade(misc)
+    upgrade(neuro)
