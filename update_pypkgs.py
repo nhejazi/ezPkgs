d1664b23d8d5d5b9056c5d972a55d0f2845b2f61
Nima Hejazi <nima.hejazi7@gmail.com>
2016-04-08 07:40:40 -0700

    Reorganize

--- update_pypkgs.py
+++ update_pypkgs.py
@@ -4,20 +4,19 @@ def upgrade(packages):
     for x in range(len(packages)):
         pip.main(['install','--upgrade',packages[x]])
 
-# Example
+# Package groupings
 core = ('ipython','Pillow','virtualenv','nose','pylint','jupyter',
-        'notebook','xonsh','neovim')
-stat = ('numpy','scipy','pandas','scikit-learn','matplotlib','seaborn',
-        'plotly','statsmodels')
+        'notebook','xonsh','neovim','awscli','StarCluster')
+stat = ('numpy','scipy','pandas','matplotlib','seaborn','plotly',
+        'scikit-learn','statsmodels')
 math = ('nltk','sympy','q','snakeviz','networkx','cloud')
-web = ('awscli','html5lib','cssselect','pyquery','lxml','BeautifulSoup')
-misc = ('Flask','Django','tornado','rdflib','SPARQLWrapper')
-neuro = ('nibabel','nipy','nitime','nilearn')
+misc = ('html5lib','cssselect','pyquery','lxml','BeautifulSoup',
+        'Flask','Django','tornado','rdflib','SPARQLWrapper')
+biol = ('nibabel','nipy','nitime','nilearn')
 
 if __name__ == '__main__':
     upgrade(core)
     upgrade(stat)
     upgrade(math)
-    upgrade(web)
     upgrade(misc)
-    upgrade(neuro)
+    upgrade(biol)
