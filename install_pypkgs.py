8ebb46271367137a16e464fb4ef0326c6da14974
Nima Hejazi <nima.hejazi7@gmail.com>
2016-04-08 07:37:50 -0700

    Add StarCluster and reorganize

--- install_pypkgs.py
+++ install_pypkgs.py
@@ -6,18 +6,17 @@ def install(packages):
 
 # Example
 core = ('ipython','Pillow','virtualenv','nose','pylint','jupyter',
-        'notebook','xonsh','neovim')
-stat = ('numpy','scipy','pandas','scikit-learn','matplotlib','seaborn',
-        'plotly','statsmodels')
+        'notebook','xonsh','neovim','awscli','StarCluster')
+stat = ('numpy','scipy','pandas','matplotlib','seaborn','plotly',
+        'scikit-learn','statsmodels')
 math = ('nltk','sympy','q','snakeviz','networkx','cloud')
-web = ('awscli','html5lib','cssselect','pyquery','lxml','BeautifulSoup')
-misc = ('Flask','Django','tornado','rdflib','SPARQLWrapper')
-neuro = ('nibabel','nipy','nitime','nilearn')
+misc = ('html5lib','cssselect','pyquery','lxml','BeautifulSoup',
+        'Flask','Django','tornado','rdflib','SPARQLWrapper')
+biol = ('nibabel','nipy','nitime','nilearn')
 
 if __name__ == '__main__':
     install(core)
     install(stat)
     install(math)
-    install(web)
     install(misc)
-    install(neuro)
+    install(biol)
