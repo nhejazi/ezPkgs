144101280a710d60e2c9322576e7e7f9bbd30449
Nima Hejazi <nima.hejazi7@gmail.com>
2016-04-19 15:40:23 -0700

    Update install_rpkgs.R

--- install_rpkgs.R
+++ install_rpkgs.R
@@ -23,7 +23,7 @@ github_pkgs <- c("hadley/staticdocs", "ramnathv/slidify",
                  "ramnathv/slidifyLibraries", "nhejazi/nima")
 
 # install packages from CRAN
-if (as.logical(grep("Darwin",Sys.info()["sysname"])) == TRUE) {
+if (as.logical(Sys.info()["sysname"] == "Darwin") == TRUE) {
   install.packages(cran_pkgs, repos = "https://cran.cnr.berkeley.edu/")
 } else {
   install.packages(cran_pkgs, repos = "http://cran.cnr.berkeley.edu/")
