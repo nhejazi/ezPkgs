123e81e3938e08727eee86ccbf6503692214beb0
Nima Hejazi <nima.hejazi7@gmail.com>
2016-04-22 16:38:32 -0700

    Update install_atompkgs.sh

--- install_atompkgs.sh
+++ install_atompkgs.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/bash
 
-apm install an-old-hope-syntax ask-stack atom-macros atomatigit autocomplete-julia autocomplete-python
+apm install an-old-hope-syntax ask-stack atom-macros atomatigit latex git-plus autocomplete-python
 
-apm install color-picker data-atom env-from-shell file-icons git-plus highlight-selected hydrogen latex
+apm install color-picker env-from-shell file-icons highlight-selected latex-completions hydrogen
 
 apm install latexer merge-conflicts language-diff language-julia language-latex language-lua minimap
 
@@ -15,5 +15,5 @@ apm install octocat-syntax open-github-from-npm pigments project-manager remote-
 # install testing stuff on Mac OS X only
 if [ `uname` == 'Darwin' ]; then
   apm install r-exec activate-power-mode travis-ci-status
-  apm install ink julia-client latex-completions  # for Julia IDE using Atom
+  apm install ink julia-ide julia-client jude  #JuliaIDE support in Atom
 fi
