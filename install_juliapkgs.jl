be89574a63449ee15b1bd5cb9660fc2073450abf
Nima Hejazi <nima.hejazi7@gmail.com>
2016-04-22 10:45:50 -0700

    Update install_juliapkgs.jl

--- install_juliapkgs.jl
+++ install_juliapkgs.jl
@@ -8,24 +8,21 @@ pkgs_progs = ["Atom","AWS","Blink","Bokeh","CodeTools","Compat","GitHub","Homebr
               "LaTeXStrings","Markdown","TexExtensions","PyCall","RCall","Lazy","LibCURL","MacroTools","ZMQ"]
 
 pkgs_maths = ["ArrayViews","Calculus","Cartesian","Distances","DualNumbers","ForwardDiff","Measures","NaNMath",
-              "Optim","Polynomials","WaveletMatrices","Wavelets","WoodburyMatrices","Contour","Cubature","Grid",
-              "Hexagons","NumericFuns","PDMats"]
+              "Optim","Polynomials","WaveletMatrices","Wavelets","WoodburyMatrices","Grid","NumericFuns","PDMats"]
 
 # packages for statistics and machine learning
 pkgs_stats = ["AverageShiftedHistograms","BinDeps","Bootstrap","CovarianceMatrices","DimensionalityReduction",
-              "Distributions","FixedEffectModels","GLM","GLMNet","GaussianMixtures","HypothesisTests","KLDivergence",
-              "KernSmooth","KernelDensity","KernelEstimator","LARS","Lasso","Loess","MCMC","MixedModels",
-              "MultivariateStats","RDatasets","StatsBase","StatsFuns","SmoothingKernels","TimeData","TimeModels",
-              "TimeSeries","CauseMap","Clustering","DecisionTree","IntervalTrees","MLBase","MachineLearning"]
+              "Distributions","GLM","GLMNet","GaussianMixtures","HypothesisTests","KLDivergence","LARS","Lasso",
+              "Loess","MCMC","MultivariateStats","RDatasets","StatsBase","StatsFuns","Clustering","DecisionTree",
+              "MLBase"]
 
 # packages for data science and data visualization
 pkgs_datsc = ["BufferedStreams","CSV","DataArrays","DataFrames","DataStreams","DataStructures","JSON","Images",
-              "ImmutableArrays","IntArrays","NullableArrays","FileIO","Hiccup","ASCIIPlots","Cairo","Gadfly",
-              "Gaston","PyPlot","Winston"]
+              "ImmutableArrays","IntArrays","NullableArrays","FileIO","Hiccup","Cairo","Gadfly","PyPlot"]
 
 # packages for miscellaneous operations
 pkgs_miscs = ["Calendar","ColorBrewer","ColorTypes","ColorVectorSpace","Colors","Compose","Dates","Nettle","Devectorize",
-              "Docile","FMIndexes","FactCheck","FixedPointNumbers","SortingAlgorithms","Yeppp","Bio","BioSeq"]
+              "Docile","FMIndexes","FactCheck","FixedPointNumbers","SortingAlgorithms"]
 
 
 # install packages iteratively from thematic lists above
