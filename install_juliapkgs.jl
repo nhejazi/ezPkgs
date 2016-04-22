952a0a86e2705fc511684ff66a31c236f9fa7c6f
Nima Hejazi <nima.hejazi7@gmail.com>
2016-04-22 16:15:03 -0700

    cleaning up package listing

--- install_juliapkgs.jl
+++ install_juliapkgs.jl
@@ -5,29 +5,37 @@ including loops to install all packages iteratively using Julia's package manage
 
 # pacakges for integrating with other software/programmatic systems
 pkgs_progs = ["Atom","AWS","Blink","Bokeh","CodeTools","Compat","GitHub","Homebrew","IJulia","Jewel",
-              "LaTeXStrings","Markdown","TexExtensions","PyCall","RCall","Lazy","LibCURL","MacroTools","ZMQ"]
-
-pkgs_maths = ["ArrayViews","Calculus","Cartesian","Distances","DualNumbers","ForwardDiff","Measures","NaNMath",
-              "Optim","Polynomials","WaveletMatrices","Wavelets","WoodburyMatrices","Grid","NumericFuns","PDMats"]
-
-# packages for statistics and machine learning
-pkgs_stats = ["AverageShiftedHistograms","BinDeps","Bootstrap","CovarianceMatrices","DimensionalityReduction",
-              "Distributions","GLM","GLMNet","GaussianMixtures","HypothesisTests","KLDivergence","LARS","Lasso",
-              "Loess","MCMC","MultivariateStats","RDatasets","StatsBase","StatsFuns","Clustering","DecisionTree",
-              "MLBase"]
+              "LaTeXStrings","Markdown","TexExtensions","PyCall","Lazy","LibCURL","MacroTools","ZMQ"]
 
 # packages for data science and data visualization
-pkgs_datsc = ["BufferedStreams","CSV","DataArrays","DataFrames","DataStreams","DataStructures","JSON","Images",
-              "ImmutableArrays","IntArrays","NullableArrays","FileIO","Hiccup","Cairo","Gadfly","PyPlot"]
+pkgs_datas = ["BufferedStreams","CSV","DataArrays","DataFrames","DataStreams","DataStructures","JSON",
+              "Images","NullableArrays","FileIO","Cairo","Gadfly","PyPlot"]
+
+# packages for basic mathematical manipulations
+pkgs_maths = ["ArrayViews","Cartesian","Distances","DualNumbers","ForwardDiff","NumericFuns","PDMats",
+              "NaNMath","Optim","Polynomials","WaveletMatrices","Wavelets","WoodburyMatrices"]
+
+# packages for statistics and machine learning
+pkgs_stats = ["AverageShiftedHistograms","Bootstrap","CovarianceMatrices","MultivariateStats","Feather",
+              "Distributions","GLM","RCall","RDatasets","ConjugatePriors","KernelDensity","PGM","Lora",
+              "RegERMs","StatsBase","StatsFuns","Clustering","MLBase","NMF"]
 
 # packages for miscellaneous operations
-pkgs_miscs = ["Calendar","ColorBrewer","ColorTypes","ColorVectorSpace","Colors","Compose","Dates","Nettle","Devectorize",
-              "Docile","FMIndexes","FactCheck","FixedPointNumbers","SortingAlgorithms"]
+pkgs_miscs = ["Calendar","ColorBrewer","ColorTypes","ColorVectorSpace","Colors","Compose","Dates",
+              "Nettle","Docile","FMIndexes","FactCheck","FixedPointNumbers"]
 
 
 # install packages iteratively from thematic lists above
 for progs=1:length(pkgs_progs)
   Pkg.add(pkgs_progs[progs])
+end
+
+for datas=1:length(pkgs_datas)
+  Pkg.add(pkgs_datas[datas])
+end
+
+for miscs=1:length(pkgs_miscs)
+  Pkg.add(pkgs_miscs[miscs])
 end
 
 for maths=1:length(pkgs_maths)
@@ -36,12 +44,4 @@ end
 
 for stats=1:length(pkgs_stats)
   Pkg.add(pkgs_stats[stats])
-end
-
-for datas=1:length(pkgs_datsc)
-  Pkg.add(pkgs_datsc[datas])
-end
-
-for miscs=1:length(pkgs_miscs)
-  Pkg.add(pkgs_miscs[miscs])
 end
