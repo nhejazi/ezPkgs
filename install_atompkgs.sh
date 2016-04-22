479691bac8532dbd51ee18262d3a6492f88dbaa6
Nima Hejazi <nima.hejazi7@gmail.com>
2016-04-22 10:38:29 -0700

    Update install_atompkgs.sh

--- install_atompkgs.sh
+++ install_atompkgs.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/bash
 
-apm install activate-power-mode an-old-hope-syntax ask-stack atom-macros atom-pair atomatigit
+apm install an-old-hope-syntax ask-stack atom-macros atomatigit autocomplete-julia autocomplete-python
 
-apm install autocomplete-julia autocomplete-paths autocomplete-python autocomplete-ruby
+apm install color-picker data-atom env-from-shell file-icons git-plus highlight-selected hydrogen latex
 
-apm install color-picker data-atom default-language env-from-shell file-icons git-plus
-
-apm install highlight-selected jupyter-notebook hydrogen latex latexer merge-conflicts
-
-apm install language-diff language-julia language-latex language-lua language-matlab-octave language-pfm 
+apm install latexer merge-conflicts language-diff language-julia language-latex language-lua minimap
 
 apm install language-r language-scala linter linter-lintr linter-pylint linter-lua linter-markdown
 
-apm install minimap minimap-find-and-replace minimap-git-diff minimap-highlight-selected minimap-pigments minimap-selection
+apm install minimap-find-and-replace minimap-git-diff minimap-highlight-selected minimap-pigments
 
-apm install octocat-syntax open-github-from-npm pigments project-manager r-exec remote-edit script travis-ci-status vim-mode
+apm install octocat-syntax open-github-from-npm pigments project-manager remote-edit script vim-mode
 
-apm install ink julia-client latex-completions
+# install testing stuff on Mac OS X only
+if [ `uname` == 'Darwin' ]; then
+  apm install r-exec activate-power-mode travis-ci-status
+  apm install ink julia-client latex-completions  # for Julia IDE using Atom
+fi
