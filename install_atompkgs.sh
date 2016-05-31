4a0753fdd47882d47c9803f5e1ab019480a47b64
Nima Hejazi <nima.hejazi7@gmail.com>
2016-05-31 15:07:15 -0700

    change Juno IDE setup to just use uber-juno

--- install_atompkgs.sh
+++ install_atompkgs.sh
@@ -1,15 +1,14 @@
 #!/usr/bin/bash
 
 # core packages to make use of Atom productive
-apm install language-r language-julia language-lua language-diff
+apm install language-r language-julia language-lua language-diff script
+apm install linter linter-lintr linter-pylint linter-lua linter-markdown
 apm install git-plus git-status git-control git-time-machine remote-edit
 apm install color-picker env-from-shell file-icons highlight-selected
-apm install linter linter-lintr linter-pylint linter-lua linter-markdown
-apm install script vim-mode minimap autocomplete-python project-manager
+apm install minimap vim-mode autocomplete-python project-manager uber-juno
 
 # additional packages to enhance productivity on OSX
 if [ `uname` == "Darwin" ]; then
-  apm install ink julia-ide julia-client jude  # Julia IDE support in Atom
   apm install language-latex language-scala latex r-exec
   apm install activate-power-mode travis-ci-status merge-conflicts
   apm install an-old-hope-syntax ask-stack open-github-from-npm atom-macros
