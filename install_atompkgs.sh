7b64c73aed6404f51f41412a2ffa63682d04bad8
Nima Hejazi <nima.hejazi7@gmail.com>
2016-05-31 15:19:04 -0700

    reorganize package layout across systems

--- install_atompkgs.sh
+++ install_atompkgs.sh
@@ -3,15 +3,14 @@
 # core packages to make use of Atom productive
 apm install language-r language-julia language-lua language-diff script
 apm install linter linter-lintr linter-pylint linter-lua linter-markdown
-apm install git-plus git-status git-control git-time-machine remote-edit
-apm install color-picker env-from-shell file-icons highlight-selected
-apm install minimap vim-mode autocomplete-python project-manager uber-juno
+apm install minimap vim-mode git-plus remote-edit file-icons color-picker
+apm install autocomplete-python travis-ci-status ask-stack merge-conflicts
+apm install env-from-shell highlight-selected project-manager uber-juno
 
 # additional packages to enhance productivity on OSX
 if [ `uname` == "Darwin" ]; then
-  apm install language-latex language-scala latex r-exec
-  apm install activate-power-mode travis-ci-status merge-conflicts
-  apm install an-old-hope-syntax ask-stack open-github-from-npm atom-macros
+  apm install language-latex language-scala latex activate-power-mode
+  apm install an-old-hope-syntax open-github-from-npm atom-macros r-exec
   apm install minimap-find-and-replace minimap-pigments minimap-cursorline
   apm install minimap-highlight-selected minimap-git-diff minimap-linter
 fi
