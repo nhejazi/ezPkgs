ef43b3a6d6e3da6a1548a572b1adfcf7f3d362d5
Nima Hejazi <nima.hejazi7@gmail.com>
2016-06-08 15:17:56 -0700

    tinkering with packages a bit more

--- install_atompkgs.sh
+++ install_atompkgs.sh
@@ -3,17 +3,16 @@
 # core packages to make use of Atom productive
 apm install pigments minimap linter highlight-selected file-icons
 apm install git-plus git-time-machine merge-conflicts color-picker
-apm install expose todo-show vim-mode script hydrogen ask-stack
-apm install csv-to-json tablr env-from-shell travis-ci-status
+apm install expose todo-show vim-mode script Hydrogen ask-stack
+apm install travis-ci-status atom-material-ui atom-material-syntax
 apm install language-r language-lua language-diff language-markdown
 apm install linter-lintr linter-pylint linter-lua linter-markdown linter-clang
-apm install autocomplete-python autocomplete-clang
-apm install atom-material-ui atom-material-syntax  # productivity theme
+apm install autocomplete-python autocomplete-julia autocomplete-clang
 apm install ink language-julia julia-client  # to setup Juno (Julia IDE)
 
 # additional packages to enhance productivity on OSX
 if [ `uname` == "Darwin" ]; then
-  apm install language-latex language-scala r-exec latex
+  apm install language-latex language-scala r-exec latex csv-to-json tablr
   apm install an-old-hope-syntax activate-power-mode  # some fun additions
   apm install minimap-find-and-replace minimap-pigments minimap-liner
   apm install minimap-cursorline minimap-highlight-selected minimap-git-diff
