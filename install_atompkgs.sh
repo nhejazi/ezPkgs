01b2f51fb948475614dc978fad1987c23d850118
Nima Hejazi <nhejazi@berkeley.edu>
2016-07-09 07:04:20 -0700

    Update install_atompkgs.sh

--- install_atompkgs.sh
+++ install_atompkgs.sh
@@ -13,18 +13,16 @@ apm install git-plus git-time-machine merge-conflicts color-picker
 apm install vim-mode expose todo-show ask-stack travis-ci-status
 apm install script Hydrogen  # run code interactively in the editor!
 apm install seti-ui seti-syntax  # a nice dark theme I actually like
+## packages to add some extra functionality to the minimap
+apm install minimap-highlight-selected minimap-find-and-replace
+apm install minimap-cursorline minimap-git-diff minimap-pigments
 ## support tools for languages, linters, autocompletion, and Juno IDE 
 apm install language-r language-lua language-diff language-markdown
-apm install autocomplete-python autocomplete-clang
+apm install language-latex autocomplete-python autocomplete-clang
 apm install linter-lintr linter-pylint linter-lua linter-markdown linter-clang
 apm install language-julia ink julia-client  # to set up Juno (Julia IDE)
 
-# additional packages to install on OS X only
+# packages that are OS X only
 if [ `uname` == "Darwin" ]; then
-  ## packages to add some extra functionality to the minimap
-  apm install minimap-highlight-selected minimap-find-and-replace
-  apm install minimap-cursorline  minimap-git-diff minimap-pigments
-  ## some extra stuff that is nice to have but totally unnecessary
-  apm install language-latex language-scala r-exec latex tablr
-  apm install an-old-hope-syntax activate-power-mode
+  apm install r-exec
 fi
