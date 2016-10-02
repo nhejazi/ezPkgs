65532ab04a2ebc3c69683fbfd509dc09ccf8fec4
Nima Hejazi <nhejazi@berkeley.edu>
2016-10-01 18:54:29 -0700

    clean up

--- install_juliapkgs.jl
+++ install_juliapkgs.jl
@@ -4,45 +4,27 @@ including loops to install all packages iteratively using Julia's package manage
 =#
 
 # pacakges for integrating with other software/programmatic systems
-pkgs_progs = ["Atom","AWS","Blink","Bokeh","CodeTools","Compat","GitHub","Homebrew","IJulia","Jewel",
-              "LaTeXStrings","Markdown","TexExtensions","PyCall","Lazy","LibCURL","MacroTools","ZMQ",
-              "Plotly"]
+pkgs_progs = ["IJulia", "Blink", "DevTools", "Compat", "Homebrew", "FileIO", "MacroTools",
+              "Lazy", "Cairo", "RCall", "PyCall"]
 
 # packages for data science and data visualization
-pkgs_datas = ["BufferedStreams","CSV","DataArrays","DataFrames","DataStreams","DataStructures","JSON",
-              "Images","NullableArrays","FileIO","Cairo","Gadfly","PyPlot"]
-
-# packages for basic mathematical manipulations
-pkgs_maths = ["ArrayViews","Cartesian","Distances","DualNumbers","ForwardDiff","NumericFuns","PDMats",
-              "NaNMath","Optim","Polynomials","WaveletMatrices","Wavelets","WoodburyMatrices"]
+pkgs_datas = ["DataArrays", "DataFrames", "Gadfly", "Winston", "PyPlot", "Plotly", "Bokeh"]
 
 # packages for statistics and machine learning
-pkgs_stats = ["AverageShiftedHistograms","Bootstrap","CovarianceMatrices","MultivariateStats","Lora",
-              "Distributions","GLM","RCall","RDatasets","ConjugatePriors","KernelDensity","PGM",
-              "RegERMs","StatsBase","StatsFuns","Clustering","MLBase","NMF"]
-
-# packages for miscellaneous operations
-pkgs_miscs = ["Calendar","ColorBrewer","ColorTypes","ColorVectorSpace","Colors","Compose","Dates",
-              "Nettle","Docile","FMIndexes","FactCheck","FixedPointNumbers"]
+pkgs_stats = ["StatsBase", "Distributions", "Distances", "MultivariateStats", "HypothesisTests",
+              "MLBase", "GLM", "GLMNet", "KernelDensity", "Clustering", "NMF", "RegERMs", "MCMC",
+              "Loess", "XGBoost", "Mocha", "ScikitLearn", "RDataSets"]
 
 
-# install packages iteratively from thematic lists above
-for progs=1:length(pkgs_progs)
-  Pkg.add(pkgs_progs[progs])
+# install packages iteratively from above lists
+for pkg = 1:length(pkgs_progs)
+  Pkg.add(pkgs_progs[pkg])
 end
 
-for datas=1:length(pkgs_datas)
-  Pkg.add(pkgs_datas[datas])
+for pkg = 1:length(pkgs_datas)
+  Pkg.add(pkgs_datas[pkg])
 end
 
-for miscs=1:length(pkgs_miscs)
-  Pkg.add(pkgs_miscs[miscs])
-end
-
-for maths=1:length(pkgs_maths)
-  Pkg.add(pkgs_maths[maths])
-end
-
-for stats=1:length(pkgs_stats)
-  Pkg.add(pkgs_stats[stats])
+for pkg = 1:length(pkgs_stats)
+  Pkg.add(pkgs_stats[pkg])
 end
