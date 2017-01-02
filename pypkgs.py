f1ac3c8b7691495e0fd436b0000550496eb48f39
Nima Hejazi <nima.hejazi7@gmail.com>
2017-01-01 22:49:52 -0800

    add jupyterthemes, rm old fxns

--- pypkgs.py
+++ pypkgs.py
@@ -1,43 +1,49 @@
-import pip, sys
+import sys
+import pip
 
-def getPkgs():
-    inst_pkgs = pip.get_installed_distributions()
-    inst_pkgs_list = sorted(["%s==%s" % (i.key, i.version) for i in inst_pkgs])
-    inst_pkgs_noVer = [c.split("==") for c in inst_pkgs_list]
-    inst_pkgs_tuple = tuple([c[0] for c in inst_pkgs_noVer])
-    return(inst_pkgs_tuple)
 
 def install(packages):
-    # loop over packages
-    for x in range(len(packages)):
-        pip.main(['install', packages[x]])
+    '''
+    install packages from a list iteratively
+    '''
+    for pkg in range(len(packages)):
+        pip.main(['install', packages[pkg]])
+
 
 def upgrade(packages):
-    for x in range(len(packages)):
-        pip.main(['install', '--upgrade', packages[x]])
+    '''
+    upgrade packages from a list iteratively
+    '''
+    for pkg in range(len(packages)):
+        pip.main(['install', '--upgrade', packages[pkg]])
+
 
 # Categorized list of packages for installation
-inst_pkgs = getPkgs()  # currently installed packages
+essentials = ('ipython', 'Pillow', 'virtualenv', 'nose', 'pylint',
+              'matplotlib')
 
-essentials = ('ipython', 'Pillow', 'virtualenv', 'nose', 'pylint', 'matplotlib')
-
-stat_core = ('numpy', 'scipy', 'pandas', 'sympy', 'scikit-learn', 'statsmodels',
-             'seaborn', 'pymc', 'pylab', 'nltk', 'q', 'snakeviz', 'networkx')
+stat_core = ('numpy', 'scipy', 'pandas', 'sympy', 'scikit-learn',
+             'statsmodels', 'seaborn', 'pymc', 'pylab', 'nltk', 'q',
+             'snakeviz', 'networkx')
 
 extras = ('altair', 'ggplot', 'plotly', 'bokeh', 'mlxtend', 'csvkit',
           'biopython', 'nibabel', 'nipy')
 
-py3only = ('jupyter', 'awscli', 'neovim', 'jedi',
+py3only = ('jupyter', 'jupyterthemes', 'awscli', 'neovim', 'jedi',
            'xonsh[ptk,proctitle]', 'gitsome')
 
 if __name__ == '__main__':
     # pip install listed packages
-    install(essentials); install(stat_core)
+    install(essentials)
+    install(stat_core)
 
     # pip upgrade listed packages
-    upgrade(essentials); upgrade(stat_core)
+    upgrade(essentials)
+    upgrade(stat_core)
 
     # essentials for use with Python v.3.3+
     if sys.version_info > (3, 4):
-        install(py3only); upgrade(py3only)
-        install(extras); upgrade(extras)
+        install(py3only)
+        install(extras)
+        upgrade(py3only)
+        upgrade(extras)
