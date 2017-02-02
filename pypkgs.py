a1500cf1c0ad7e2e65eeb77c26f959efd4f8086b
Nima Hejazi <nh@nimahejazi.org>
2017-02-02 08:27:14 -0800

    add yapf, reorg

--- pypkgs.py
+++ pypkgs.py
@@ -20,29 +20,27 @@ def upgrade(packages):
 
 # Categorized list of packages for installation
 essentials = ('ipython', 'Pillow', 'virtualenv', 'nose', 'pylint', 'numpy',
-              'matplotlib', 'pandas')
+              'matplotlib')
 
-stat_core = ('scipy', 'scikit-learn', 'statsmodels', 'seaborn', 'pymc', 'pylab',
-             'sympy', 'nltk', 'q', 'snakeviz', 'networkx')
+stat_core = ('pandas', 'scipy', 'scikit-learn', 'statsmodels', 'seaborn',
+             'pymc', 'pylab', 'sympy', 'nltk', 'q', 'snakeviz', 'networkx')
 
 extras = ('altair', 'ggplot', 'plotly', 'bokeh', 'mlxtend', 'csvkit',
           'biopython', 'nibabel', 'nipy')
 
 py3only = ('jupyter', 'jupyterthemes', 'awscli', 'neovim', 'jedi', 'gitless',
-           'xonsh[ptk,proctitle]', 'gitsome')
+           'xonsh[ptk,proctitle]', 'gitsome', 'yapf')
 
 if __name__ == '__main__':
-    # pip install listed packages
+    # install only bare minimum on all Python versions
     install(essentials)
-    install(stat_core)
-
-    # pip upgrade listed packages
     upgrade(essentials)
-    upgrade(stat_core)
 
-    # essentials for use with Python v.3.3+
-    if sys.version_info > (3, 4):
+    # install full suite of tools on Python v.3.5+ only
+    if sys.version_info > (3, 5):
+        install(stat_core)
         install(py3only)
         install(extras)
+        upgrade(stat_core)
         upgrade(py3only)
         upgrade(extras)
