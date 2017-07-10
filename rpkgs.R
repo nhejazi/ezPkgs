518a1ec797af9984d45861a325b50b0652dd087c
Nima Hejazi <nh@nimahejazi.org>
2017-07-10 16:04:43 -0700

    re-org pkgs

--- rpkgs.R
+++ rpkgs.R
@@ -1,19 +1,14 @@
 #!/usr/bin/env Rscript
 
 # lists of packages to install, organized by repository
-cran_pkgs <- c("Rcpp", "RcppEigen", "plyr", "dplyr", "pryr", "tidyr",
-               "lintr", "knitr", "RCurl", "purrr", "ggplot2", "car",
-               "devtools", "xtable", "pander", "testthat", "foreign",
-               "lubridate", "shiny", "stringr", "data.table", "nlme",
-               "lme4", "reshape2", "roxygen2", "littler", "ggthemes",
-               "DBI", "yaml", "fastICA", "statmod", "glmnet", "mlr",
-               "randomForest", "e1071", "multcomp", "caret", "ggvis",
-               "plotly", "mgcv", "rmarkdown", "gridExtra", "xgboost",
-               "ProjectTemplate", "scatterplot3d", "profvis", "tmle",
-               "R.devices", "future", "tibble", "shiny", "sparklyr",
-               "rbokeh", "dtplyr", "elasticnet", "stargazer", "h2o",
-               "rdrop2", "dbplyr", "bartMachine", "ranger", "Rtsne",
-               "fst", "prophet", "timekit", "tidyquant"
+cran_pkgs <- c("Rcpp", "RcppEigen", "plyr", "pryr", "lintr", "knitr", "RCurl",
+               "car", "xtable", "pander", "foreign", "data.table", "nlme",
+               "lme4", "roxygen2", "littler", "ggthemes", "DBI", "yaml",
+               "fastICA", "statmod", "glmnet", "mlr", "randomForest", "e1071",
+               "multcomp", "caret", "ggvis", "plotly", "mgcv", "gridExtra",
+               "xgboost", "ProjectTemplate", "scatterplot3d", "profvis", "tmle",
+               "future", "rbokeh", "elasticnet", "stargazer", "h2o", "rdrop2",
+               "bartMachine", "ranger", "Rtsne", "fst", "prophet"
               )
 
 bioc_pkgs <- c("GenomicRanges", "GenomicFeatures", "GenomeInfoDb", "biomaRt",
@@ -25,13 +20,32 @@ bioc_pkgs <- c("GenomicRanges", "GenomicFeatures", "GenomeInfoDb", "biomaRt",
                "scran", "SC3", "clusterExperiment"
               )
 
-github_pkgs <- c("krlmlr/here", "ramnathv/slidify", "ramnathv/slidifyLibraries",
-                 "ramnathv/htmlwidgets", "jalvesaq/colorout", "hadley/pkgdown",
-                 "rstudio/bookdown", "ecpolley/SuperLearner", "jimhester/covr",
-                 "karthik/wesanderson", "yihui/xaringan", "rlbarter/superheat",
-                 "tidyverse/broom", "yihui/servr", "cloudyr/rmote",
-                 "ThinkRstat/fcuk"
+github_pkgs <- c("business-science/timekit", "business-science/tidyquant",
+                 "cloudyr/rmote", "ecpolley/SuperLearner", "gaborcsardi/prompt",
+                 "jalvesaq/colorout", "jimhester/covr",  "karthik/wesanderson",
+                 "krlmlr/here", "krlmlr/styler", "nhejazi/nima",
+                 "rlbarter/superheat", "ThinkRstat/fcuk", "yihui/xaringan",
+                 "wesm/feather/R"
                 )
+
+hadley_pkgs <- paste("hadley", sep = "/",
+                     c("devtools", "testthat", "pkgdown")
+                    )
+
+rstudio_pkgs <- paste("rstudio", sep = "/",
+                      c("rmarkdown", "bookdown", "blogdown", "rticles", "shiny",
+                        "packrat", "sparklyr", "tensorflow", "keras")
+                     )
+
+tidyverse_pkgs <- paste("tidyverse", sep = "/",
+                        c("ggplot2", "dplyr", "purrr", "readr", "forcats",
+                          "stringr", "dtplyr", "dbplyr", "tidyr", "tibble",
+                          "broom", "lubridate", "tidyverse")
+                       )
+
+rlib_pkgs <- paste("r-lib", sep = "/",
+                   c("rcmdcheck", "callr", "usethis")
+                  )
 
 # put all packages in a common folder (prevents R version issues)
 if (as.character(Sys.info()["effective_user"]) == "nimahejazi") {
@@ -50,12 +64,6 @@ biocLite(bioc_pkgs)
 biocLite(ask = FALSE)
 
 # install packages from GitHub
-library(devtools)
-devtools::install_github(github_pkgs, force = TRUE)
-devtools::install_github("nhejazi/nima", ref = "develop")
-
-# add packages for the R kernel in Jupyter
-install.packages(c('repr', 'IRdisplay', 'evaluate', 'crayon', 'pbdZMQ',
-                   'devtools', 'uuid', 'digest'))
-devtools::install_github("IRkernel/IRkernel")
-IRkernel::installspec(user = FALSE)
+devtools::install_github(c(github_pkgs, hadley_pkgs, rstudio_pkgs,
+                           tidyverse_pkgs, rlib_pkgs),
+                         force = TRUE)
