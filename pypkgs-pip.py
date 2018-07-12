74de93329dc1286017dcc4b1bcf8114e9fda9765
Nima Hejazi <nh@nimahejazi.org>
2018-07-12 15:13:46 -0700

    revise python packages

--- pypkgs-pip.py
+++ pypkgs-pip.py
@@ -19,26 +19,26 @@ def upgrade(packages):
 
 
 # Categorized list of packages for installation
-essential = ('ipython', 'Pillow', 'virtualenv', 'nose', 'pylint', 'numpy')
+core = ('ipython', 'Pillow', 'virtualenv', 'pipenv', 'nose', 'pylint')
 
-core = ('matplotlib', 'pandas', 'numba', 'scipy', 'scikit-learn', 'seaborn',
-        'statsmodels', 'pymc', 'pylab', 'sympy', 'nltk', 'q', 'snakeviz',
-        'networkx', 'tensorflow', 'keras', 'torchvision', 'altair', 'ggplot',
-        'plotly', 'bokeh', 'mlxtend', 'csvkit', 'dask', 'distributed')
+stats = ('numpy', 'matplotlib', 'pandas', 'numba', 'scipy', 'scikit-learn',
+         'statsmodels', 'lifelines', 'pymc', 'pylab', 'sympy', 'nltk', 'q',
+         'snakeviz', 'altair', 'ggplot', 'plotly', 'bokeh', 'seaborn',
+         'mlxtend', 'csvkit', 'dask', 'distributed')
 
-extra = ('jupyter', 'jupyterlab', 'jupyter_kernel_gateway', 'jedi', 'yapf',
-         'awscli', 'neovim', 'gitless', 'xonsh', 'gitsome', 'rtichoke')
+system = ('jupyter', 'jupyterlab', 'jupyter_kernel_gateway', 'jedi', 'yapf',
+          'neovim', 'xonsh', 'rtichoke')
 
 
 if __name__ == '__main__':
     # install only bare minimum on all Python versions
-    install(essential)
-    upgrade(essential)
+    install(core)
+    upgrade(core)
 
     # install full suite of tools on Python v.3.5+ only
     if sys.version_info > (3, 5):
-        install(core)
-        install(extra)
-        upgrade(core)
-        upgrade(extra)
+        install(stats)
+        install(system)
+        upgrade(stats)
+        upgrade(system)
 
